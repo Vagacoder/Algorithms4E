1d1080276decf45b540f8da330403a3c813e0a15
Vagacoder <qiruihu@gmail.com>
2019-11-17 10:17:16 -0800

    update add launch.json for other computer

--- javasrc/ch00/QuickStart.java
+++ javasrc/ch00/QuickStart.java
@@ -6,5 +6,6 @@ class QuickStart {
     public static void main(String[] args) {
         System.out.println("Hello World");
         StdOut.print("Hello Java");
+        StdOut.println("Hello again!");
     }
 }
