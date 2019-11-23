17a164c6be606622e1b1f5840284869444ca40e3
Vagacoder <qiruihu@gmail.com>
2019-11-22 22:29:59 -0800

    update stddraw tester x3

---
+++ javasrc/ch01_1/StdDrawTester01.java
@@ -0,0 +1,19 @@
+package javasrc.ch01_1;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class StdDrawTester01{
+
+    public static void main(String[] args){
+        int N = 100;
+        StdDraw.setXscale(0, N);
+        StdDraw.setYscale(0, N*N);
+        StdDraw.setPenRadius(0.01);
+
+        for (int i=1; i<= N; i++){
+            StdDraw.point(i, i);
+            StdDraw.point(i, i*i);
+            StdDraw.point(i, i*Math.log(i));
+        }
+    }
+}
---
+++ javasrc/ch01_1/StdDrawTester03.java
@@ -0,0 +1,25 @@
+package javasrc.ch01_1;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class StdDrawTester03{
+
+    public static void main(String[] args){
+        int N = 50;
+        double[] a = new double[N];
+        for(int i=0; i< N; i++){
+            a[i] = StdRandom.random();
+        }
+        Arrays.sort(a);
+        for(int i=0; i<N; i++){
+            double x= 1.0*i/N;
+            double y= a[i]/2.0;
+            double rw = 0.2/N;
+            double rh = a[i]/2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+}
