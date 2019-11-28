8214c0a688436ce8f98c7f2fc6258216cd2ad6cf
Vagacoder <qiruihu@gmail.com>
2019-11-27 16:54:17 -0800

    update ex1.3

---
+++ javasrc/ch01_1/ex1_3.java
@@ -0,0 +1,17 @@
+package javasrc.ch01_1;
+
+/*
+
+*/
+import lib.StdIn;
+import lib.StdOut;
+
+public class ex1_3 {
+
+    public static void main(String[] args) {
+        StdOut.print("Please enter 3 integers:");
+
+        StdIn.readInt();
+
+    }
+}
