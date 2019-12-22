7a4f2ddeb8b6249198117a50516c9af1a3c7349a
Vagacoder <qiruihu@gmail.com>
2019-12-22 09:43:42 -0800

    working on 1.3.38

---
+++ javasrc/ch01_3/GeneralizedQueue.java
@@ -0,0 +1,89 @@
+package javasrc.ch01_3;
+
+/*
+1.3.38 Delete kth element. Implement a class that supports the following API:
+
+boolean isEmpty(): is the queue empty?
+void insert(Item x): add an item
+Item delete(int k): delete and return the kth least recently inserted item
+
+Note: this class is useing an array implementation
+*/
+
+import lib.StdOut;
+
+public class GeneralizedQueue<T>{
+
+    private T[] queue;
+    private int numberInQueue;
+    private int first;
+    private int last;
+
+    public GeneralizedQueue(){
+        @SuppressWarnings("unchecked")
+        T[] q = (T[]) new Object[4];
+        this.queue = q;
+        this.numberInQueue = 0;
+        this.first = 0;
+        this.last = 0;  // last exclusive
+    }
+
+    public boolean isEmpty(){
+        return this.numberInQueue == 0;
+    }
+
+    public int size(){
+        return this.numberInQueue;
+    }
+
+    private void resize(int newCapacity){
+        assert newCapacity > this.numberInQueue;
+        @SuppressWarnings("unchecked")
+        T[] q = (T[]) new Object[newCapacity];
+        for(int i = 0; i< this.numberInQueue; i++){
+            q[i] = this.queue[this.first + i];
+        }
+        this.queue = q;
+        this.first = 0;
+        this.last = this.numberInQueue;
+    }
+
+    public void insert(T newItem){
+        if(this.last >= this.queue.length){
+            this.resize(this.numberInQueue * 2);
+        }
+        this.queue[this.last++] = newItem;
+        this.numberInQueue++;
+    }
+
+    public T delete(int k){
+
+        return null;
+    }
+
+    public void print(){
+        int length = this.queue.length;
+        for(int i = 0; i< length; i++){
+            if(this.queue[i] == null){
+                StdOut.print("null ");
+            } else{
+                StdOut.print(this.queue[i] + " ");
+            }
+        }
+        StdOut.println();
+    }
+
+    public static void main(String[] args){
+        StdOut.println("test");
+        GeneralizedQueue<String> gq = new GeneralizedQueue<>();
+        gq.insert("we");
+        gq.print();
+        StdOut.println("size: " + gq.size());
+        gq.insert("are");
+        gq.insert("good");
+        gq.insert("at");
+        gq.insert("study");
+        gq.print();
+        StdOut.println("size: " + gq.size());
+    }
+}
--- javasrc/ch01_3/Josephues.java
+++ javasrc/ch01_3/Josephues.java
@@ -4,7 +4,7 @@ package javasrc.ch01_3;
 1.3.37 Josephus problem. 
 In the Josephus problem from antiquity, N people are in dire straits and agree to 
 the following strategy to reduce the population. They arrange themselves in a 
-circle (at positions numbered from 0 to N–1) and proceed around the circle,
+circle (at positions numbered from 0 to N-1) and proceed around the circle,
 eliminating every Mth person until only one person is left. Legend has it that 
 Josephus figured out where to sit to avoid being eliminated. 
 
