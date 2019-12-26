6b4bf4727ed2611b4f6393f4e5e0a63be9a38a0d
Vagacoder <qiruihu@gmail.com>
2019-12-25 17:13:14 -0800

    update 1.3.43 with better tree view

--- javasrc/ch01_3/ListFiles.java
+++ javasrc/ch01_3/ListFiles.java
@@ -16,7 +16,7 @@ import java.io.File;
 
 public class ListFiles {
 
-    // Solution 1: Not using queue
+    // Solution 1: Not using queue, simple tree view ===========================
     public static void listAllFiles1(String pathName) {
         File thisPath = new File(pathName);
         if (thisPath.isFile()) {
@@ -48,8 +48,44 @@ public class ListFiles {
         }
     }
 
-    // Solution 2: using queue
+    // Solution 2: No queue, better tree view ==================================
     public static void listAllFiles2(String pathName) {
+        File thisPath = new File(pathName);
+        StdOut.println(pathName);
+        if (thisPath.isDirectory()) {
+            listAllFilesWithIndent1(pathName, "");
+        }
+    }
+
+    // helper for solution 2
+    private static void listAllFilesWithIndent1(String folderName, String leadingString) {
+        File thisFolder = new File(folderName);
+
+        String[] fileList = thisFolder.list();
+        int listLength = fileList.length;
+        for (int i = 0; i < listLength; i++) {
+            String fileName = fileList[i];
+            if (i != listLength - 1) {
+                StdOut.println(leadingString + " \u251C\u2500\u2500\u2500\u2500\u2500 " + fileName);
+            } else {
+                StdOut.println(leadingString + " \u2514\u2500\u2500\u2500\u2500\u2500 " + fileName);
+            }
+            File thisFile = new File(folderName + File.separator + fileName);
+
+            if (thisFile.isDirectory()) {
+                String newLeadingString = leadingString;
+                if (i != listLength - 1) {
+                    newLeadingString += " \u2502      ";
+                } else {
+                    newLeadingString += "\t";
+                }
+                listAllFilesWithIndent1(folderName + File.separator + fileName, newLeadingString);
+            }
+        }
+    }
+
+    // Solution 3: using queue =================================================
+    public static void listAllFiles3(String pathName) {
         File thisPath = new File(pathName);
         if (thisPath.isFile()) {
             StdOut.println(pathName);
@@ -77,11 +113,19 @@ public class ListFiles {
 
         // Test for solution 1
         StdOut.println("1. testing solution 1 ...");
-        ListFiles.listAllFiles1("./");
+        // ListFiles.listAllFiles1("./");
         // ListFiles.listAllFiles1("./README.md");
-        // ListFiles.listAllFiles("./data");
-        // ListFiles.listAllFiles("./javasrc");
+        // ListFiles.listAllFiles1("./data");
+        // ListFiles.listAllFiles1("./javasrc");
         StdOut.println("End of 1 ... \n");
+
+        // Test for solution 2
+        StdOut.println("2. testing solution 2 ...");
+        ListFiles.listAllFiles2("./");
+        // ListFiles.listAllFiles2("./README.md");
+        // ListFiles.listAllFiles2("./data");
+        // ListFiles.listAllFiles2("./javasrc");
+        StdOut.println("End of 2 ... \n");
 
     }
 }
