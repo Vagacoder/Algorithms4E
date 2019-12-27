fb88a855bcf69e346657da5591fa3a63740d205f
Vagacoder <qiruihu@gmail.com>
2019-12-26 17:07:16 -0800

    ex1.4.11 is done

--- javasrc/ch01_2/StaticSETofInts.java
+++ javasrc/ch01_2/StaticSETofInts.java
@@ -1,6 +1,13 @@
 package javasrc.ch01_2;
 
+/*
+1.4.11 Add an instance method howMany() to StaticSETofInts (page 99) that finds the
+number of occurrences of a given key in time proportional to log N in the worst case.
+
+*/
+
 import java.util.Arrays;
+import lib.StdOut;
 
 public class StaticSETofInts {
 
@@ -33,5 +40,69 @@ public class StaticSETofInts {
             }
         }
         return -1;
+    }
+
+    public int howMany(int key) {
+        int low = 0;
+        int high = a.length - 1;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key < a[mid]) {
+                high = mid - 1;
+            } else if (key > a[mid]) {
+                low = mid + 1;
+            } else {
+                index = mid;
+                break;
+            }
+        }
+
+        if (index < 0) {
+            return 0;
+        } else {
+            int lowestIndex = findLowest(key, low, index);
+            int highestIndex = findHighest(key, index, high);
+            return highestIndex - lowestIndex + 1;
+        }
+    }
+
+    private int findLowest(int key, int low, int high) {
+        if (low >= high - 1) {
+            return a[low] == key ? low : high;
+        }
+        int mid = (low + high) / 2;
+        if (key > this.a[mid]) {
+            return findLowest(key, mid, high);
+        } else {
+            return findLowest(key, low, mid);
+        }
+    }
+
+    private int findHighest(int key, int low, int high) {
+        if (low >= high - 1) {
+            return a[high] == key ? high : low;
+        }
+        int mid = (low + high) / 2;
+        if (key < this.a[mid]) {
+            return findHighest(key, low, mid);
+        } else {
+            return findHighest(key, mid, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 6, 6, 6, 7, 8, 9, 9, 9 };
+        StaticSETofInts set = new StaticSETofInts(a);
+        StdOut.println("Expext 2 (of 1): " + set.howMany(1));
+        StdOut.println("Expext 1 (of 2): " + set.howMany(2));
+        StdOut.println("Expext 3 (of 3): " + set.howMany(3));
+        StdOut.println("Expext 1 (of 4): " + set.howMany(4));
+        StdOut.println("Expext 2 (of 5): " + set.howMany(5));
+        StdOut.println("Expext 4 (of 6): " + set.howMany(6));
+        StdOut.println("Expext 1 (of 7): " + set.howMany(7));
+        StdOut.println("Expext 1 (of 8): " + set.howMany(8));
+        StdOut.println("Expext 3 (of 9): " + set.howMany(9));
     }
 }
---
+++ javasrc/ch01_4/ex1_4_9.java
@@ -0,0 +1,54 @@
+package javasrc.ch01_4;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+
+/*
+ * 1.4.8 Write a program to determine the number pairs of values in an input
+ * file that are equal. If your first try is quadratic, think again and use
+ * Arrays.sort() to develop a linearithmic solution.
+ * 
+ */
+
+import lib.StdOut;
+
+public class ex1_4_9 {
+
+    public static void FindEqualPairs1(int[] ints) {
+        int count = 0;
+        int length = ints.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                // int a = (ints[i]);
+                // int b = (ints[j]);
+                if (ints[i] == ints[j]) {
+                    StdOut.println(ints[i] + " " + ints[j]);
+                    count++;
+                }
+            }
+        }
+
+        StdOut.println("Total pairs are: " + count);
+    }
+
+    public static void FindEqualPairs2(int[] ints) {
+        Arrays.sort(ints);
+        int length = ints.length;
+        int count = 0;
+        for (int i = 0; i < length - 1; i++) {
+            int j = i + 1;
+            while (ints[i] == ints[j]) {
+                count++;
+                j++;
+            }
+        }
+        StdOut.println("Total pairs are: " + count);
+    }
+
+    public static void main(String[] args) {
+        In in = new In("./data/32KintsWithPairs.txt");
+        int[] input = in.readAllInts();
+        FindEqualPairs2(input);
+    }
+}
