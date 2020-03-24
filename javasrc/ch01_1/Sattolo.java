404a7b41cef1bfbf1621e1a4fcc1231f21d48a88
Vagacoder <qiruihu@gmail.com>
2020-03-23 17:38:27 -0700

    3/23 working on web1.1.1

---
+++ javasrc/ch01_1/Sattolo.java
@@ -0,0 +1,26 @@
+package javasrc.ch01_1;
+
+/*
+* Web Ex. 1.1.1. Sattolo's algorithm. 
+
+Write a program Sattolo.java that generates a unifomly distributed cycle of length N 
+using Sattolo's algorithm. 
+
+? https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Sattolo.27s_algorithm
+
+*/
+
+import lib.*;
+
+public class Sattolo{
+
+    private Sattolo(){}
+
+    public static void shuffle(Object[] a){
+
+    }
+
+    public static void main(String[] args){
+    
+    }
+}
