86e2c5f77b63d0793543e44210028a8b741c39c0
Vagacoder <qiruihu@gmail.com>
2020-11-22 17:24:46 -0800

    11/22 update brute force string search

--- javasrc/ch05_3/BruteForce.java
+++ javasrc/ch05_3/BruteForce.java
@@ -10,25 +10,47 @@ import lib.*;
 
 public class BruteForce {
 
-    public static int search(String txt, String pattern){
+    public static int search(String pattern, String txt) {
         int M = pattern.length();
         int N = txt.length();
 
-        for(int i = 0; i < N-M; i++){
+        for (int i = 0; i < N - M; i++) {
             int j;
-            for(j =0; j < M;j++){
-                if(txt.charAt(i+j) != pattern.charAt(j)){
+            for (j = 0; j < M; j++) {
+                if (txt.charAt(i + j) != pattern.charAt(j)) {
                     break;
                 }
             }
-            if(j == M){
+            if (j == M) {
                 return i;
             }
         }
         return N;
     }
 
-    public static void main(String[] args){
+    // * this method is a bridge to KMP algorithm
+    public static int search2(String pattern, String txt) {
+        int i;
+        int j;
+        int M = pattern.length();
+        int N = txt.length();
+
+        for (i = 0, j = 0; i < N && j < M; i++) {
+            if (txt.charAt(i) == pattern.charAt(j)) {
+                j++;
+            } else {
+                i -= j; // ! important ! i points to the end of sequence already matched.
+                j = 0;
+            }
+        }
+        if (j == M) {
+            return i - M;
+        } else {
+            return N;
+        }
+    }
+
+    public static void main(String[] args) {
         String text = "This is a good day to die.";
         String pattern = "is";
         int index = search(text, pattern);
